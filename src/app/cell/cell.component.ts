4cb0b0d53cfe1ada776e4b3c18c758a53d40f029
Andrew Wilborn <andrew.j.wilborn@sbcglobal.net>
2023-08-28 17:04:52 -0400

    MODIFIED conditional for cell component

--- src/app/cell/cell.component.ts
+++ src/app/cell/cell.component.ts
@@ -9,4 +9,9 @@ export class CellComponent {
   @Input() cellVal!: string;
   @Input() numberCellVal!:number;
   numberArr: string[] = ["bg-blank", "bg-one", "bg-two", "bg-three", "bg-four", "bg-five", "bg-six", "bg-seven", "bg-eight"];
+
+  isNumber(x:string){
+    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
+    return numbers.includes(x)
+  }
 }
