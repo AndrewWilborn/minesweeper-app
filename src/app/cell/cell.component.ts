21ada143a01a14268a100ae9380327649d285e0e
Andrew Wilborn <andrew.j.wilborn@sbcglobal.net>
2023-08-28 17:38:14 -0400

    ADDED onclick to cell component

--- src/app/cell/cell.component.ts
+++ src/app/cell/cell.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cell',
@@ -9,10 +9,20 @@ export class CellComponent {
   @Input() board!: string;
   @Input() cellId!: number;
   @Input() inGame!: boolean;
+  @Output() stringChanged = new EventEmitter<string>();
   numberArr: string[] = ["bg-blank", "bg-one", "bg-two", "bg-three", "bg-four", "bg-five", "bg-six", "bg-seven", "bg-eight"];
 
   isNumber(x:string){
     const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
     return numbers.includes(x)
+  }
+
+  onStringChange() {
+    this.stringChanged.emit(this.board);
+  }
+
+  testMove() {
+    console.log("hello")
+    this.board = this.board.substring(0, this.cellId) + '8' + this.board.substring(this.cellId+1)
   }
 }
