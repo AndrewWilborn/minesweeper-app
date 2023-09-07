c9208c6374e6e2896b7f3d3d57e99386d4d5a27f
Andrew Wilborn <andrew.j.wilborn@sbcglobal.net>
2023-09-07 14:03:09 -0400

    ADDED functional mine counter

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -7,8 +7,9 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class BoardComponent {
   board: string = "                                                                                                                                                                                                                                                                "
-  flagLocations: boolean[] = []
+  flagLocations: boolean[] = [];
   gameId: string = "";
+  minesLeft: Number = 50;
   idArray(x: number) {
     let returnArr = [];
     for (let i = 0; i < x; i++) {
@@ -25,6 +26,11 @@ export class BoardComponent {
   }
   onFlagChanged(newFlags: boolean[]) {
     this.flagLocations = newFlags;
+    let numFlags = 0;
+    for(let i = 0; i < this.flagLocations.length; i++){
+      if(this.flagLocations[i]) numFlags++;
+    }
+    this.minesLeft = 50 - numFlags;
   }
 
   flagMode: boolean = false;
